8463890edbe43027b3247be2d52ab4657f517a47
Martin Gonzalez <>
2017-12-23 16:05:34 -0300

    static method to call for a svg in shield.io

--- lib/badge/generator.rb
+++ lib/badge/generator.rb
@@ -4,16 +4,9 @@ class BadgeGenerator
 
   URL_PREFIX = 'https://img.shields.io/badge/'.freeze
 
-  def badge(label = 'label', message ='message', color = 'grey', style = 'flat-square')
-    escapedUri = URI.escape(URL_PREFIX + "#{label}-#{message}-#{color}.svg")
+  def self.badge(label = 'label', message ='message', color = 'grey', style = 'flat-square')
+    escapedUri = URI.escape(URL_PREFIX + "#{label}-#{message}-#{color}?style=#{style}.svg")
     uri = URI.parse(escapedUri)
     Net::HTTP.get(uri)
   end
-
 end
-
-
-
-a = BadgeGenerator.new
-
-puts a.badge
